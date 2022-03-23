a07e87dfb78485468ed42db92b3ceb364a3398d5
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-03-23 14:10:51 +0300

    Разбивает код на модули

---
+++ js/util.js
@@ -0,0 +1,17 @@
+const getRandomNumber = (min, max) => {
+  if (min < 0 || max < min) {
+    return null;
+  }
+
+  return min + Math.round(Math.random() * (max - min));
+};
+
+const getRandomFrom = (array) => {
+  const randomIndex = getRandomNumber(0, array.length - 1);
+
+  return array[randomIndex];
+};
+
+const doesNotExceed = (string, maxLength) => string.length <= maxLength;
+
+export { getRandomNumber, getRandomFrom, doesNotExceed };
