d32b233ab476886f2134bd672ad74e3efda1b2cc
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-09-15 20:57:49 +0300

    Заменяет временные данные на данные с сервера

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,22 +1,41 @@
-import { getImages } from './data.js';
+import { getData } from './fetch.js';
 
 const picturesArea = document.querySelector('.pictures');
+
 const pictureTemplate = document
   .querySelector('#picture')
   .content.querySelector('.picture');
 
-const fragment = document.createDocumentFragment();
-const picturesData = getImages(25);
+const warningTemplate = document
+  .querySelector('#warning')
+  .content.querySelector('.warning');
 
-picturesData.forEach(({ id, url, likes: likesCount, comments }) => {
-  const picture = pictureTemplate.cloneNode(true);
-  picture.dataId = id;
-  picture.querySelector('.picture__img').src = url;
-  picture.querySelector('.picture__comments').textContent = comments.length;
-  picture.querySelector('.picture__likes').textContent = likesCount;
-  fragment.appendChild(picture);
-});
+const onError = () => {
+  document.body.appendChild(warningTemplate);
+  setTimeout(() => {
+    warningTemplate.style.opacity = 0;
+  }, 5000);
+};
 
-picturesArea.appendChild(fragment);
+const onSuccess = (data) => {
+  const fragment = document.createDocumentFragment();
+
+  data.forEach(({ id, url, likes: likesCount, comments }) => {
+    const picture = pictureTemplate.cloneNode(true);
+    picture.dataId = id;
+    picture.querySelector('.picture__img').src = url;
+    picture.querySelector('.picture__comments').textContent = comments.length;
+    picture.querySelector('.picture__likes').textContent = likesCount;
+    fragment.appendChild(picture);
+  });
+
+  picturesArea.appendChild(fragment);
+};
+
+const picturesData = await getData(
+  'https://24.javascript.pages.academy/kekstagram/data',
+  onError,
+  onSuccess
+);
 
 export { picturesArea, picturesData };
