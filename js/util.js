290b8d9459376d0a9eca9a77b4a0ef5746955d64
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-09-15 21:27:19 +0300

    Удаляет механизм генерации временных данных

--- js/util.js
+++ js/util.js
@@ -1,27 +1,6 @@
-const getRandomNumber = (min, max) => {
-  if (min < 0 || max < min) {
-    return null;
-  }
-
-  return min + Math.round(Math.random() * (max - min));
-};
-
-const getRandomFrom = (array) => {
-  const randomIndex = getRandomNumber(0, array.length - 1);
-
-  return array[randomIndex];
-};
-
 const doesExceed = (sequence, maxLength) => sequence.length > maxLength;
 const matchPattern = (value, pattern) => pattern.test(value);
 const hasDuplicates = (arr) => new Set(arr).size < arr.length;
 const isEscapeKey = (evt) => evt.code === 'Escape';
 
-export {
-  getRandomNumber,
-  getRandomFrom,
-  doesExceed,
-  matchPattern,
-  hasDuplicates,
-  isEscapeKey,
-};
+export { doesExceed, matchPattern, hasDuplicates, isEscapeKey };
